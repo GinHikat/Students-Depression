91b02e4da7248d72437905b24dfbc419fc8186a1
GinHikat <phamduyanh0816@gmail.com>
2025-03-22 13:14:38 +0700

    Update README

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-import sys
-import os
-import pandas as pd
-
-df= pd.read_csv('main/data/ingested/train.csv')
-
-x = 'Depression'
-
-target = df[[x]]
-
-print(target)
-
+'''
+Test will be taken here to catch bugs
+'''
